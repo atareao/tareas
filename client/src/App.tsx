35b92c3c30b0ea9d2c1f301ee00318f7ac25fcd7
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-01-13 09:18:29 +0100

    :package: Agregadas nuevas dependencias y actualizada la configuración del tema en App.tsx para implementar @toolpad/core y mejorar la navegación de la aplicación

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import * as React from 'react';
+import { createTheme } from '@mui/material/styles';
+import { PageContainer } from '@toolpad/core/PageContainer';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { useDemoRouter } from '@toolpad/core/internal';
+import Paper from '@mui/material/Paper';
 
-function App() {
-  const [count, setCount] = useState(0)
+const NAVIGATION = [
+    { segment: '', title: 'Home' },
+    { segment: 'orders', title: 'Orders' },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const router = useDemoRouter('/orders');
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+
+    return (
+            <AppProvider navigation={NAVIGATION} router={router} theme={darkTheme}>
+                <Paper sx={{ width: '100%' }}>
+                    {/* preview-start */}
+                    <PageContainer>Page content</PageContainer>
+                    {/* preview-end */}
+                </Paper>
+            </AppProvider>
+    );
 }
-
-export default App
