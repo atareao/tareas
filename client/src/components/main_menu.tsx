a741b4b3bc8adedba91840ea8b1fdc75c61d9067
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-01-15 07:26:40 +0100

    ✨ Añadido nuevo componente de formulario para mejorar la captura de datos

---
+++ client/src/components/main_menu.tsx
@@ -0,0 +1,38 @@
+
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export default function ScrollableTabsButtonForce() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Container>
+            <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons
+                    allowScrollButtonsMobile
+                    aria-label="scrollable force tabs example"
+                >
+                    <Tab onClick={()=>{console.log("Item One")}}label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                    <Tab label="Item Four" />
+                    <Tab label="Item Five" />
+                    <Tab label="Item Six" />
+                    <Tab label="Item Seven" />
+                </Tabs>
+            </Box>
+        </Container>
+    );
+}
+
