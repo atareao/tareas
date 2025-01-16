d4ede859e794208d3cddf38ee7f776d8cee414bf
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-01-16 07:33:45 +0100

    🎨 Refactoriza main_menu.tsx: convierte componente funcional a clase y añade botón de creación de lista

--- client/src/components/main_menu.tsx
+++ client/src/components/main_menu.tsx
@@ -3,36 +3,82 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
-export default function ScrollableTabsButtonForce() {
-    const [value, setValue] = React.useState(0);
+export default class ScrollableTabsButtonForce extends React.Component {
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
+    state = {
+        value: 0,
+    }
 
-    return (
-        <Container>
-            <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    variant="scrollable"
-                    scrollButtons
-                    allowScrollButtonsMobile
-                    aria-label="scrollable force tabs example"
+    constructor(props: any) {
+        super(props);
+        this.state = { value: 0 };
+    }
+
+    handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        this.setState({ value: newValue });
+    }
+
+    getButtonFirstList() {
+        console.log("Create List");
+        return (
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Button
+                    onClick={() => { console.log("Create List") }}
+                    variant="contained"
+                    color="primary"
+                    size="small"
                 >
-                    <Tab onClick={()=>{console.log("Item One")}}label="Item One" />
-                    <Tab label="Item Two" />
-                    <Tab label="Item Three" />
-                    <Tab label="Item Four" />
-                    <Tab label="Item Five" />
-                    <Tab label="Item Six" />
-                    <Tab label="Item Seven" />
-                </Tabs>
+                    Create First List
+                </Button>
             </Box>
-        </Container>
-    );
-}
+        );
+    }
 
+    getTabs() {
+        return (
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box
+                    sx={{
+                        maxWidth: { xs: 320, sm: 480 },
+                        bgcolor: 'background.paper'
+                    }}
+                >
+                    <Tabs
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        variant="scrollable"
+                        scrollButtons
+                        allowScrollButtonsMobile
+                        aria-label="scrollable force tabs example"
+                    >
+                        <Tab onClick={() => { console.log("Item One") }} label="Item One" />
+                        <Tab label="Item Two" />
+                        <Tab label="Item Three" />
+                        <Tab label="Item Four" />
+                        <Tab label="Item Five" />
+                        <Tab label="Item Six" />
+                        <Tab label="Item Seven" />
+                    </Tabs>
+                </Box>
+            </Box>
+        );
+
+    }
+
+    render() {
+        if (true) {
+            return this.getButtonFirstList();
+        }
+        return this.getTabs();
+    }
+}
